45acf8ece9dacf40c8169a34ab5d4d1002e61d8a
Chavva Teja Venkata Ratna Sai Kumar <83270390+TejaChavva123@users.noreply.github.com>
2023-06-22 09:13:05 +0530

    Update uploadRoutes.js

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 // Set up storage for uploaded images
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, process.env.UPLOADS_FOLDER);
   },
   filename: function (req, file, cb) {
     cb(
